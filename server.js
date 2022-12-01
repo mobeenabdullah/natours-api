057433243b3f85e9c86be3ee9baa5fb23832ec57
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-01 22:44:15 +0500

    Setting up ESLint + Prettier in VS Code

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server is running on port ${port}`);
 });
