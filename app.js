c62b2d2c72afdcf6e848d86acbf06cb5005adb80
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-01 08:57:17 +0500

    Handling PATCH Requests

--- app.js
+++ app.js
@@ -30,8 +30,6 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.get('/api/v1/tours/:id', (req, res) => {
-  console.log(req.params);
-
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
 
@@ -51,7 +49,6 @@ app.get('/api/v1/tours/:id', (req, res) => {
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
@@ -68,6 +65,22 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
 });
 
 const port = 3000;
