b649acbac7ed1a74659827ceff0ad4d4ef73d2b9
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-03 01:03:25 +0500

    Making the API Better: Aliasing

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -8,13 +8,12 @@ const {
   createTour,
   updateTour,
   deleteTour,
-  // checkID,
-  // checkBody,
+  aliasTopTours,
 } = tourController;
 
 const router = express.Router();
 
-// router.param('id', checkID);
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
