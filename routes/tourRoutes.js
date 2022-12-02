77128d73fde76c021b37f6cb6078b51009948739
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-02 09:41:55 +0500

    Refactoring for MVC

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const tourController = require('./../controllers/tourController');
+const tourController = require('../controllers/tourController');
 
 const {
   getAllTours,
@@ -8,13 +8,13 @@ const {
   createTour,
   updateTour,
   deleteTour,
-  checkID,
+  // checkID,
   checkBody,
 } = tourController;
 
 const router = express.Router();
 
-router.param('id', checkID);
+// router.param('id', checkID);
 
 router.route('/').get(getAllTours).post(checkBody, createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
