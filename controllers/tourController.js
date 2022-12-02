c6feb900212c38b66ff2262b1c3077e2b376d69a
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-03 00:14:23 +0500

    Making the API Better: Advanced Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,25 +2,29 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    // Build the query
+    console.log(req.query);
+
+    // BUILD QUERY
+    // 1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    console.log(req.query, queryObj);
+    // 2) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    const query = Tour.find(queryObj);
+    const query = Tour.find(JSON.parse(queryStr));
 
+    // EXECUTE QUERRY
+    const tours = await query;
     // const tours = await Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
 
-    // Execute the query
-    const tours = await query;
-
-    // Send response
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
