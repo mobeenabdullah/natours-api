7efb9f8b5c96f7394b701733c812a4045faf89a0
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-08 02:28:26 +0500

    Logging in Users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,11 +6,12 @@ const authController = require('../controllers/authController');
 const { getAllUsers, getUser, createUser, updateUser, deleteUser } =
   userController;
 
-const { signup } = authController;
+const { signup, login } = authController;
 
 const router = express.Router();
 
 router.post('/signup', signup);
+router.post('/login', login);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
