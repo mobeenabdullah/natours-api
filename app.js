8c81a37db321d8b9db2340d6007c29f50988db4b
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-08 02:42:21 +0500

    Protecting Tour Routes - Part 1

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  // console.log('HEADERS', req.headers);
+
   next();
 });
 
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const tourController = require('../controllers/tourController');
+const authController = require('../controllers/authController');
 
 const {
   getAllTours,
@@ -13,6 +13,8 @@ const {
   getMonthlyPlan,
 } = tourController;
 
+const { protect } = authController;
+
 const router = express.Router();
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
@@ -21,7 +23,7 @@ router.route('/tour-stats').get(getTourStats);
 
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(protect, getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
