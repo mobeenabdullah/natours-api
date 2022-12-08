5e84addf38fb69182198be5145dc4e6678eb96fa
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-08 23:32:19 +0500

    Updating the Current User: Password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,14 @@ const authController = require('../controllers/authController');
 const { getAllUsers, getUser, createUser, updateUser, deleteUser } =
   userController;
 
-const { signup, login, forgotPassword, resetPassword } = authController;
+const {
+  signup,
+  login,
+  forgotPassword,
+  resetPassword,
+  protect,
+  updatePassword,
+} = authController;
 
 const router = express.Router();
 
@@ -15,6 +22,8 @@ router.post('/login', login);
 
 router.post('/forgotPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
+
+router.patch('/updateMyPassword', protect, updatePassword);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
