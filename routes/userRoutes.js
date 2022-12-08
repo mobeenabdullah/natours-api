9ea3781c113059ffd5fc5ead0c6756307ed80b67
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-08 14:36:59 +0500

    Password Reset Functionality: Reset Token

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,12 +6,15 @@ const authController = require('../controllers/authController');
 const { getAllUsers, getUser, createUser, updateUser, deleteUser } =
   userController;
 
-const { signup, login } = authController;
+const { signup, login, forgotPassword, resetPassword } = authController;
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
+
+router.post('/forgotPassword', forgotPassword);
+router.patch('/resetPassword', resetPassword);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
