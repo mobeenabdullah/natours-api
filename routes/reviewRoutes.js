3dbe98272515cc715c9937544d6f791f2dd2abaf
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-10 01:53:16 +0500

    Creating and Getting Reviews

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const reviewController = require('../controllers/reviewController');
+const authController = require('../controllers/authController');
+
+const { getAllReviews, createReview } = reviewController;
+const { protect, restrictTo } = authController;
+
+const router = express.Router();
+
+router
+  .route('/')
+  .get(getAllReviews)
+  .post(protect, restrictTo('user'), createReview);
+
+module.exports = router;
