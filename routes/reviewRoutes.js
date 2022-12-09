c8cd1c0b9689c8809fd180600360868bc6a71170
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-10 04:47:20 +0500

    Building Handler Factory Functions: Delete

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
-const { getAllReviews, createReview } = reviewController;
+const { getAllReviews, createReview, deleteReview } = reviewController;
 const { protect, restrictTo } = authController;
 
 const router = express.Router({
@@ -13,5 +13,7 @@ router
   .route('/')
   .get(getAllReviews)
   .post(protect, restrictTo('user'), createReview);
+
+router.route('/:id').delete(deleteReview);
 
 module.exports = router;
