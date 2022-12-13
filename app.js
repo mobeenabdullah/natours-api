23c87df3c9f4c2faa74ad5a2f08e6ce91a625de8
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-13 22:53:57 +0500

    Creating Our Base Template & Including Files into Pug Templates

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
   res.status(200).render('base', {
     tour: 'The Forest Hiker',
-    user: 'Jonas',
+    user: 'Mobeen',
   });
 });
 
