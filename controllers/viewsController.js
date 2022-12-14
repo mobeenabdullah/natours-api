6dbce8b5d62159871531fc704aa8661a36351e24
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-14 10:31:42 +0500

    Building the Login Screen

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -25,9 +25,14 @@ exports.getTour = catchAsync(async (req, res) => {
 
   // 2) Build template
   // 3) Render template using data from 1)
-
   res.status(200).render('tour', {
     title: `${tour.name} Tour`,
     tour,
   });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account',
+  });
+};
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const viewController = require('../controllers/viewsController');
 
-const { getOverview, getTour } = viewController;
+const { getOverview, getTour, getLoginForm } = viewController;
 
 const router = express.Router();
 
 router.get('/', getOverview);
 router.get('/tour/:slug', getTour);
+router.get('/login', getLoginForm);
 
 module.exports = router;
