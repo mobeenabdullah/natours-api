363f7572e3123f0512844338dad204ebbc8d2c72
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-14 15:15:43 +0500

    Logging in Users with Our API - Part 2

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const viewController = require('../controllers/viewsController');
+const authController = require('../controllers/authController');
 
 const { getOverview, getTour, getLoginForm } = viewController;
+const { isLoggedIn } = authController;
 
 const router = express.Router();
+
+router.use(isLoggedIn);
 
 router.get('/', getOverview);
 router.get('/tour/:slug', getTour);
