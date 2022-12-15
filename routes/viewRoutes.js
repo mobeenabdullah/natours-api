a82a0a0085c165133a3fa7458b67207fa080c445
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-16 00:11:42 +0500

    Updating User Data (old way)

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const viewController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
 
-const { getOverview, getTour, getLoginForm, getAccount } = viewController;
+const { getOverview, getTour, getLoginForm, getAccount, updateUserData } =
+  viewController;
 const { isLoggedIn, protect } = authController;
 
 const router = express.Router();
@@ -11,5 +12,7 @@ router.get('/', isLoggedIn, getOverview);
 router.get('/tour/:slug', isLoggedIn, getTour);
 router.get('/login', isLoggedIn, getLoginForm);
 router.get('/me', protect, getAccount);
+
+router.post('/submit-user-data', protect, updateUserData);
 
 module.exports = router;
