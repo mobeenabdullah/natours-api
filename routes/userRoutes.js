88daf4825b8a0f6fc1d85147529ecbe3bc4bce6c
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-15 22:26:29 +0500

    Logging out Users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -22,12 +22,14 @@ const {
   protect,
   restrictTo,
   updatePassword,
+  logout,
 } = authController;
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.get('/logout', logout);
 
 router.post('/forgotPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
