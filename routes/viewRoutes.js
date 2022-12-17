b247a56ef77e7eb4f22499c99f583aa42f5195df
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-17 11:16:06 +0500

    Creating New Bookings on Checkout Success

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const viewController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const { getOverview, getTour, getLoginForm, getAccount, updateUserData } =
   viewController;
 const { isLoggedIn, protect } = authController;
 
+const { createBookingCheckout } = bookingController;
+
 const router = express.Router();
 
-router.get('/', isLoggedIn, getOverview);
+router.get('/', createBookingCheckout, isLoggedIn, getOverview);
 router.get('/tour/:slug', isLoggedIn, getTour);
 router.get('/login', isLoggedIn, getLoginForm);
 router.get('/me', protect, getAccount);
