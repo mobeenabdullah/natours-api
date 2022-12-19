95735320b82c5da1947f936597e5907e29be6fe4
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-19 20:01:51 +0500

    Improved Stripe Implementation

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const viewController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
-const bookingController = require('../controllers/bookingController');
 
 const {
   getOverview,
@@ -13,11 +12,9 @@ const {
 } = viewController;
 const { isLoggedIn, protect } = authController;
 
-const { createBookingCheckout } = bookingController;
-
 const router = express.Router();
 
-router.get('/', createBookingCheckout, isLoggedIn, getOverview);
+router.get('/', isLoggedIn, getOverview);
 router.get('/tour/:slug', isLoggedIn, getTour);
 router.get('/login', isLoggedIn, getLoginForm);
 router.get('/me', protect, getAccount);
